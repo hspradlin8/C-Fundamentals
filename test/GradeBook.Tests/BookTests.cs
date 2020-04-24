87d94612a24c38099033ad60d08d6dd89446f0e4
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2020-04-24 17:39:47 -0500

    unitTesting

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange section
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            //act section
+            var result = book.GetStatistics();
+
+            // assert section
+            Assert.Equals(85.6, result.Average, 1);
+            Assert.Equals(90.5, result.High, 1);
+            Assert.Equals(77.3, result.Low, 1);
+
+        }
+    }
+}
